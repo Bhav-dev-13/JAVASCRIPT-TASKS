783542af466a49b61f292fcaa153bf361fe88264
Bhav-dev-13 <Kotikalapudibhavya@gmail.com>
2025-01-27 01:59:09 +0530

    JS tasks

---
+++ Day2/secondjs.js
@@ -0,0 +1,71 @@
+//Simple declaration of {var, let, const}
+var a1 = 1;
+console.log(a1)
+let variable2 = "Good Morning!";
+console.log(variable2)
+const c3 = false;
+console.log(c3)
+
+// Automatic Variable
+d=56;
+console.log(d)
+lol5 = "WoW!!!";
+console.log(lol5)
+
+// Hoisting
+b = 78;
+console.log(b)
+var b;
+
+// Re-assigning of a variable
+var pb = 45;
+pb = 67;
+console.log(pb)
+
+// Undefined
+var string; //Not an error its an datatype
+console.log(string)
+
+// Datatypes[Primitive]
+var a = 10; //Number
+console.log(a)
+var b = "Bhavss" //String
+console.log(b)
+var c;//Undefined
+console.log(c)
+var d1 = null; //Null
+console.log(d1)
+var e5 = true; //Boolean
+console.log(e5)
+
+// Non-primitive Datatypes
+var arr = [1,"hello",true,false] //Array
+console.log(arr)
+// By reference
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[3])
+// Object : provide clear information
+var obj = {
+    id:1,
+    name:"Bhavya",
+    batchno:89
+}
+// Accessing particular object element
+console.log(obj.id)
+console.log(obj.name)
+console.log(obj.batchno)
+
+// To Know about the datatype
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d1))
+console.log(typeof(e5))
+
+// Not defined
+console.log(u)
+
+
+
